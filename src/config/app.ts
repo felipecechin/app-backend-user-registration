938bb9438be07cb6d5cebb2c11f5f2f2ab4cc480
Felipe Cechin Mello <ficechin@hotmail.com>
2022-12-06 13:31:10 -0300

    updated app config

--- src/config/app.ts
+++ src/config/app.ts
@@ -19,7 +19,7 @@ createConnection()
 const app = express()
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'content-type,Authorization')
     next()
